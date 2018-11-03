b5019b91e7c317f0ee520ce7c7014929d10eafd0
aanfaisal <aan.faisalal@gmail.com>
2018-11-04 00:49:31 +0700

    softdelete

--- database/migrations/2018_10_13_090251_create_tasks_table.php
+++ database/migrations/2018_10_13_090251_create_tasks_table.php
@@ -18,6 +18,7 @@ class CreateTasksTable extends Migration
             $table->string('title') ;
             $table->text('description');
             $table->timestamps();
+            //$table->SoftDelete();
         });
     }
 
